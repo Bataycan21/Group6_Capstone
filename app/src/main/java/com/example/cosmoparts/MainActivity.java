caa719425821cdd5a6db476db9d7c2fb3196f609
Bataycan21 <158477898+Bataycan21@users.noreply.github.com>
2024-05-26 03:25:49 +0800

    part1

---
+++ app/src/main/java/com/example/cosmoparts/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.cosmoparts;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+
+import com.example.cosmoparts.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationView.setBackground(null);
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            if (item.getItemId() == R.id.home) {
+                replaceFragment(new HomeFragment());
+            } else if (item.getItemId() == R.id.computer) {
+                replaceFragment(new ComputerFragment());
+            } else if (item.getItemId() == R.id.mouse) {
+                replaceFragment(new PeripheralsFragment());
+            } else if (item.getItemId() == R.id.cart) {
+                replaceFragment(new CartFragment());
+            }
+            return true;
+
+
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+}
